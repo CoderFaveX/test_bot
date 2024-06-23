337980307dd6e6bc87227eef3a298ff03ec0b40c
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-23 17:53:24 +0100

    updating

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+$path = "https://api.telegram.org/bot6905825214:AAEOuNYP5bXnX6cFMCVIr4AQAlGWiMs8N-I";
+$update = json_decode(file_get_contents("php://input"), TRUE);
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+if (strpos($message, "/testing") === 0) {
+    $name = substr($message, 9);
+    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=Here's your name:  " . $name);
+}
